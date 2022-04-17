4dc1be001498b8158dcfad5e5c370828e82106d8
Adrian Tobi <Adriantobi@users.noreply.github.com>
2022-04-17 23:17:32 +0100

    hereItIs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This is a sample Python script.
 import cv2
-import numpy
+import numpy as np
 
 
 cv2.namedWindow("preview")
@@ -11,12 +11,23 @@ if vc.isOpened(): # try to get the first frame
 else:
     rval = False
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    key = cv2.waitKey(27)
-    if key == 27: # exit on ESC
+while(1):
+    _, frame = vc.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+    lower_red = np.array([30,150,50])
+    upper_red = np.array([255,255,180])
+    
+    mask = cv2.inRange(hsv, lower_red, upper_red)
+    res = cv2.bitwise_and(frame,frame, mask= mask)
+
+    cv2.imshow('frame',frame)
+    cv2.imshow('mask',mask)
+    cv2.imshow('res',res)
+    
+    k = cv2.waitKey(5) & 0xFF
+    if k == 27:
         break
 
+cv2.destroyAllWindows()
 vc.release()
-cv2.destroyWindow("preview")
